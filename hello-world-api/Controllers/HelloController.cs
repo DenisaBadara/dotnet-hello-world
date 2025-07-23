0d81808004f4ec31da0ca38ca6e3047532e25e79
DenisaBadara <147556820+DenisaBadara@users.noreply.github.com>
2025-07-23 22:49:03 +0300

    Update HelloController.cs

--- hello-world-api/Controllers/HelloController.cs
+++ hello-world-api/Controllers/HelloController.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace hello_world_api.Controllers
 {
-    [Route("api/[controller]")]
-    public class HelloController : Controller
+    [ApiController]
+    [Route("/")] 
+    public class HelloController : ControllerBase
     {
-        // GET api/helllo
         [HttpGet]
         public string Get()
         {
             return "Hello world!!!!!!";
         }
-
     }
 }
+
